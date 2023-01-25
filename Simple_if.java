a35eb65e7b87dde9898840da0897d30553a5423b
issach315 <117745188+issach315@users.noreply.github.com>
2023-01-25 15:51:29 +0530

    telling logic

--- Simple_if.java
+++ Simple_if.java
@@ -7,7 +7,7 @@ public class Simple_if
         // in this progarm we used the simple if contional staement in java
         for(int i=1 ;i<=10 ;i++)
         {
-            if (i != 5)
+            if (i != 5)// logic is escaping the 5 and printing the all the inteers from loop
             {
                 System.out.println(i);
             }
